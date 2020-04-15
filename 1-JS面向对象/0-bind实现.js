88878882e5c8acbab04714ecb93875a692d8f618
EmilyYoung71415 <775363160@qq.com>
2020-04-15 23:58:45 +0800

    practice17-call\bind实现优化

--- 1-JS面向对象/0-bind实现.js
+++ 1-JS面向对象/0-bind实现.js
@@ -1,38 +1,72 @@
 /*****
  * Function.prototype.bind()
  *          function.bind(thisArg[, arg1[, arg2[, ...]]])
- *          1. 给调用函数的this强制绑定为thisArg
- *          2. 返回新的函数，不直接执行函数，手动调用新函数
- *          3. 传入参数
+ *          1. 返回新的函数，不直接执行函数，手动调用新函数 => 返回新函数
+ *          2. 给调用函数的this强制绑定为thisArg => call函数
+ *          3. 传入参数 => arguments 处理 bind的参数  和 调用参数, 两次参数concat一下
+ *          4. [重点]
+ *             当 bind 返回的函数fn作为构造函数的时候，bind 时指定的 this 值失效(传入的参数不失效)
+ *             var newFunc = Chinese.bind(Asian);
+ *             let obj = new newFunc();  // console.log(this.skin) 应该输出为undefined
+ *                                       // 因为此时this指向构造函数本身Chinese
  */
-// demo1
-// var Asian = {
-//     skin: 'yellow'
-// };
-// function Chinese() {
-//     console.log(this.skin);
-// }
-// // 返回了一个函数
-// var newFunc = Chinese.bind(Asian); 
-// newFunc(); // yellow
 
-Function.prototype._bind = function (context) {
 
-    if (typeof this !== "function") {
-        throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
+ /***
+ * 核心：
+ * bind和call的不同之处:
+ *     var func = bind(this, arg1, arg2..)
+ *     func(arg3,arg4...)
+ * 1. bind传入this，返回新函数，调用新函数生效，并不调用原函数
+ * 2. bind可以接受两次传值：bind时，调用时，需要将这两次的传值都传递到原函数接受到
+ */
+// base版
+Function.prototype._bind = function (ctx) {
+    var self = this; // 函数调用者 借用方法的借用人
+    return function () {
+        return self.apply(ctx);
     }
+};
 
-    var self = this;
-    var args = Array.prototype.slice.call(arguments, 1);
 
-    var fNOP = function () {};
+/***
+ * 遗漏的case：
+ * 1、可以接受传参，且两次传参
+ * 2、bind生成的函数 可以new 作为构造函数调用
+ *    当作为构造函数new时，bind强制绑定的this应该失效
+ */
 
-    var fBound = function () {
-        var bindArgs = Array.prototype.slice.call(arguments);
-        return self.apply(this instanceof fNOP ? this : context, args.concat(bindArgs));
-    };
+Function.prototype.$bind = function () {
+    var args = [].slice.call(arguments); // 类数组转数组 bind(ctx, arg1, arg2..)
+    var ctx = args.shift();
+    var self = this; // 函数调用者 借用方法的借用人
+    var fbind = function () {
+        var bindArgs = [].slice.call(arguments);
+        var _this = this instanceof fbind ? this : ctx;
+        return self.apply(_this, args.concat(bindArgs));
+    }
+    // 当new bind生成的函数时func = new [Chinese.bind(xxx)]() 期望的是 Chinese上的 属性:this 和 原型上的
+    // 由于bind返回新函数，所以会断开构造器与实例之间的联系，因此bind函数内部需要手动建立联系
+    var F = function () {};
+    F.prototype = self.prototype;
+    fbind.prototype = new F();
+    return fbind;
+};
 
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-    return fBound;
+var Asian = {
+    skin: 'yellow'
 }
+
+function Chinese(a, b, c, d) {
+    this.color = 'red';
+    console.log(this.skin);
+    console.log([a, b, c, d]);
+    console.log(this.color);
+    console.log(this.test);
+}
+Chinese.prototype.test = 1;
+
+// Chinese.$call(Asian, 1, 2, 3, 4);
+let func = Chinese.$bind(Asian, 1, 2);
+let p = new func(3,4);
+console.log(p.constructor)
--- 1-JS面向对象/0-call、apply实现.js
+++ 1-JS面向对象/0-call、apply实现.js
@@ -28,38 +28,54 @@
  *      3、当执行的函数fn()有返回值时，call方法调用后也得有返回值
  */
 
-Function.prototype._call = function (ctx) {
-    ctx.fn = this;
-    ctx.fn();
-    var args = [];
-    for (var i = 1, len = arguments.length; i < len; i++) {
-        args.push('arguments[' + i + ']');
-    }
-
-    // 使用eval将拼接的字符当作js执行：ctx.fn(arguments[1], arguments[2], ...);
-    var result = eval('context.fn(' + args + ')');
-
-    delete ctx.fn;
-    return result;
-};
-
-
-// apply 同理 只是参数不一样
-Function.prototype._apply = function (ctx, arr) {
-    ctx.fn = this;
-
-    var result;
-    if (!arr) {
-        result = ctx.fn();
-    }
-    else {
-        var args = [];
-        for (var i = 0, len = arr.length; i < len; i++) {
-            args.push('arr[' + i + ']');
-        }
-        result = eval('context.fn(' + args + ')');
-    }
-
+// 核心思想: base 版
+Function.prototype._call = function (ctx, ...arg) {
+    ctx.fn = this; // this是函数调用者 希望借用方法的借用人
+    let result = ctx.fn(...arg); // 参数数组展开 传入函数
     delete ctx.fn;
     return result;
 }
+
+/**
+ * 遗漏的重要的case:
+ * 1、ctx传入为null时，默认指向window
+ * 2、...args 扩展符是es6才有的，需要想办法将数组转换为一个个的参数 传递给函数调用
+ *      => var result = ctx.fn(args.join(',')); 这样展开不行
+ *         因为join之后是一个字符串'1,2,3,4' 而不是fn(1,2,3,4)
+ *         所以引入了eval 动态执行代码
+ * 3、ctx上原本就有fn？
+ */
+//  完整版
+Function.prototype.$call = function (ctx) {
+    ctx = ctx || window;
+    // var args = [].slice.call(arguments); // 其他参数 类数组转数组
+    ctx.fn = this; // this是函数调用者 希望借用方法的借用人
+    // let result = ctx.fn(...args); // 参数数组展开 传入函数
+
+    var args = [];
+    for (var i = 1; i < arguments.length; i++) {
+        args.push(arguments[i]);
+    }
+    // context.fn(arguments[1], arguments[2], ...);
+    // 在eval中，args 自动调用 args.toString()方法
+    var result = eval('context.fn(' + args +')');
+    delete ctx.fn;
+    return result;
+}
+
+var Asian = {
+    skin: 'yellow'
+}
+
+function Chinese(a, b, c, d) {
+    console.log(this.skin);
+    console.log([a, b, c, d]);
+}
+
+Chinese.$call(Asian, 1, 2, 3, 4);
+
+// let arr = [1,2,3];
+// eval('console.log('+ [arr[1], arr[2]] +')');
+
+
+// apply 同理 只是参数不一样
