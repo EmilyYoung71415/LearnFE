ddd195b895261bfe73f7dbe773e78d5ed628b340
EmilyYoung71415 <775363160@qq.com>
2020-06-01 23:56:45 +0800

    异步并发返回结果-尤大优化版 & 终极清爽版

--- 2-异步/1-异步并发按顺序返回.js
+++ 2-异步/1-异步并发按顺序返回.js
@@ -34,18 +34,112 @@ function getInfo(i, cb) {
   *     这样就可以按顺序返回了
   */
 
-(function getDataAsync1() {
-    let result = [];
-    let count = 0;
-    for (let i = 0; i < 5; i++) {
+// (function getDataAsync1() {
+//     let result = [];
+//     let count = 0;
+//     for (let i = 0; i < 5; i++) {
+//         getInfo(i, function (res) {
+//             result[i] = res;
+//             count++;
+//             if (count === 5) {
+//                 for (let j = 0; j < 5; j++) {
+//                     console.log(result[j]);
+//                 }
+//             }
+//         });
+//     }
+// })();
+
+
+ /****************************************************************
+  * 优化1(尤大实现版)
+  *        way1的不足之处: 等全部结果返回后才开始打印，期间用户白屏的时间过长
+  *                       能否实现既按顺序打印 又即时打印的效果呢
+  *        思路：
+  *             异步回调里：当前异步是第一个异步 则无条件打印
+  *                        elseif 当前为第i个 则判断i-1个是否已返回结果并打印
+  *                        （这样解决的是 当前异步主动盘查前一个是否有结果再打印
+  *                         还需要 当前异步有结果后 主动盘查后一个是否有结果再打印
+  */
+
+(function getDataAsync2() {
+    const total = 5;
+
+    let responses = [];
+    let displayed = [];
+
+    for (let i = 0; i < total; i++) {
         getInfo(i, function (res) {
-            result[i] = res;
-            count++;
-            if (count === 5) {
-                for (let j = 0; j < 5; j++) {
-                    console.log(result[j]);
-                }
+            responses[i] = res;
+            // only display if
+            // - this is the first request
+            // or
+            // - the previous request has been displayed
+            if (i === 0 || displayed[i - 1]) {
+                display(i);
             }
-        });
+        })
+    }
+
+    function display (index) {
+        console.log(responses[index]);
+        displayed[index] = true;
+
+        if (index === total - 1) {
+            // all done.
+        } else if (responses[index + 1]) {
+            // recursively display the next request if it has been received.
+            display(index + 1);
+        }
+    }
+})();
+
+ /****************************************************************
+  * 优化1(终极版 清晰清爽版)
+  *        way1的不足之处: 等全部结果返回后才开始打印，期间用户白屏的时间过长
+  *                       能否实现既按顺序打印 又即时打印的效果呢
+  *        思路：
+  *             异步回调里：当前异步是第一个异步 则无条件打印
+  *                        elseif 当前为第i个 则判断i-1个是否已返回结果并打印
+  *                        （这样解决的是 当前异步主动盘查前一个是否有结果再打印
+  *                         还需要 当前异步有结果后 主动盘查后一个是否有结果再打印（且是递归式盘查 链式的
+  * 
+  *         优化： 每次cb回调之后 递归式盘查 当前i之后的异步是否已有结果值
+  */
+
+
+ (function getDataAsync3() {
+    const total = 5;
+
+    let responses = [];
+    let displayedIndex = 0; // 全局维护当前输出结果的index 每次输出后自增1
+
+    for (let i = 0; i < total; i++) {
+        getInfo(i, function (res) {
+            // responses[i] = res;
+            // if (i === 0 || displayed[i - 1]) {
+            //     display(i);
+            // }
+            handleResult(res, i);
+        })
+    }
+
+    function handleResult (res, index) {
+        responses[index] = res;
+        // console.log(responses[index]);
+        // displayed[index] = true;
+
+        // if (index === total - 1) {
+        //     // all done.
+        // } else if (responses[index + 1]) {
+        //     // recursively display the next request if it has been received.
+        //     display(index + 1);
+        // }
+
+        // 是否输出 就遍历res数组
+        while (responses[displayedIndex]) {
+            console.log(responses[displayedIndex]);
+            displayedIndex++;
+        }
     }
 })();
