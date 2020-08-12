fc107c6f1ab410f9bb8c5391678a0b345891b875
EmilyYoung71415 <775363160@qq.com>
2020-08-12 23:46:38 +0800

    h5-template-vuecli4 init适配

---
+++ 6-webpack/h5-template-vuecli4/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    <i class="icon logo"></i>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
