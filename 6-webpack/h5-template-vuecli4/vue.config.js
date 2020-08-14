7ba6de3e4a67da0892972b34338b8fb4da920340
EmilyYoung71415 <775363160@qq.com>
2020-08-15 00:13:55 +0800

    请求跨域解决: 本地proxy代理

--- 6-webpack/h5-template-vuecli4/vue.config.js
+++ 6-webpack/h5-template-vuecli4/vue.config.js
@@ -1,13 +1,36 @@
-const path = require('path');
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
+    devServer: {
+        // overlay: { // 让浏览器 overlay 同时显示警告和错误
+        //   warnings: true,
+        //   errors: true
+        // },
+        // open: false, // 是否打开浏览器
+        // host: "localhost",
+        // port: "8080", // 代理断就
+        // https: false,
+        // hotOnly: false, // 热更新
+        proxy: {
+            "/api": {
+                target: "https://zhidao.baidu.com", // 目标代理接口地址
+                secure: false,
+                changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
+                // ws: true, // 是否启用websockets
+                pathRewrite: {
+                    "^/api": "/"
+                }
+            }
+        }
+    },
     chainWebpack: config => {
+        // 添加别名
         config.resolve.alias
-
             .set('@', resolve('src'))
-            .set('@assets', resolve('src/assets'))
-            .set('@components', resolve('src/components'))
-            .set('@views', resolve('src/views'))
-            .set('@static', resolve('src/static'));
+            .set('assets', resolve('src/assets'))
+            .set('api', resolve('src/api'))
+            .set('views', resolve('src/views'))
+            .set('components', resolve('src/components'))
     }
-};
+}
