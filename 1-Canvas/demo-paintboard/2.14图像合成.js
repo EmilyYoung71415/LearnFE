280eef9d04f55c7900dbb456701e9034652a5a96
EmilyYoung71415 <775363160@qq.com>
2021-04-26 09:44:53 +0800

    canvas：坐标变换应用

---
+++ 1-Canvas/demo-paintboard/2.14图像合成.js
@@ -0,0 +1,3 @@
+// ctx.globalCompositeOperation = 'source-over' | 'source-in' | 'source-out' | 'source-atop' | 'destination-over'
+// 规定新图像如何绘制在旧图像上，一般默认是新的盖在旧的上面
+// 这个设置可以实现 混合，反切等效果 TODO: 按需学习暂时用不到
